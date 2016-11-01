a2c7e71326c7999ac1ded4e838ed4ede4673aaa8
moanam <moanam@clear.net.nz>
2016-11-01 14:38:03 -0400

    Changed column names, seperated by repicate and summed abundances from different water masses. A few attempts follow

--- Heat map.R
+++ Heat map.R
@@ -18,3 +18,36 @@ tax = mydata[,c(2,35)]
 colnames(tax) = c("OTU", "Taxonomy")
 
 map = mydata[,c(3,15,16,13)]
+colnames(map) = c("Sample", "RevBarcode", "FwdBarcode", "Timepoint")
+
+########################################################################################################################
+# Separate data by replicate
+dat.M1 = subset(mydata, Replicate == "A")
+dat.M2 = subset(mydata, Replicate == "B")
+
+
+dat.M1 = dat.M1[,c(1,3,12)]
+
+####This sums the OTU abundances from different water masses from the same time
+
+library(dplyr)
+
+df <- group_by(dat.M1, OTU, Time)
+
+df.summary <- summarise(df, Abundance = sum(value))
+
+library(dplyr)
+res <- dat.M1 %>%
+  group_by(OTU, Time) %>% 
+  mutate(value=sum(value))
+dat.M3 = as.data.frame(res)
+
+dat.M4 = aggregate(OTU ~ Time, data=dat.M1, sum)
+
+dat.M4 = aggregate(dat.M1, OTU)
+
+
+
+head(dat.M1)
+
+dat.M3 <- data.frame(dat.M1[,-1], row.names=dat.M1[,1]) #This is currently producing duplicate row names as I have each OTU for each time point
