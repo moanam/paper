1fc0190bf3f08280629637f3bb1128a7e949a0e8
moanam <moanam@clear.net.nz>
2016-11-01 16:25:52 -0400

    Updated which functions file to use so I can make changes

--- Heat map.R
+++ Heat map.R
@@ -11,7 +11,7 @@ file.path = "C:/Users/Moana/Documents/Uni/2016/Publication/"
 tax.path = paste(file.path, "Fasta file/outseqs_by_sample_Moana.fasta", sep="")
 # abs.path = paste(file.path, "Data/Absolute_abundances/particleTrajs.txt", sep="")
 out.path = paste(file.path, "Output/", sep="")
-source(paste(file.path, "fig2_v1_functions.R", sep=""))
+source(paste(file.path, "paper/fig2_v1_functions.R", sep=""))
 
 mydata = read.csv("C:/Users/Moana/Documents/Uni/2016/Publication/paper/Fulltaxonomy.csv", header = T)
 tax = mydata[,c(2,35)]
