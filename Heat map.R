c548c766c822c309dd2369e8c948a1b7b24c524d
moanam <moanam@clear.net.nz>
2016-11-06 14:16:37 -0700

    I made a data frame with what I think is the OTU taxonomy that I later plot. Now I need to match this with the orders and redo the figure

--- Heat map.R
+++ Heat map.R
@@ -154,7 +154,15 @@ for (i in 1:length(reps)){
   # Prepare for plotting taxonomic distinctions
 #  tax$Taxonomy <- as.character(tax$Taxonomy)
   #famNames = unlist(lapply(row.names(mat2plot), function(OTU) strsplit(tax[which(tax$OTU == OTU), "Taxonomy"], "|", fixed=TRUE)[[1]][4]))
-  famNames <- tax[['Taxonomy']]
+  famNames <- tax[['Taxonomy']] #This takes all the taxonomy. I think mat2plot has the OTUs in my figure
+  
+  library(data.table)
+  OTUs_in_fig = setDT(mat2plot, keep.rownames = TRUE)[] #Makes what I think are the oTUs a column
+  OTUnames = subset(OTUs_in_fig, select=c("rn")) #Isolate the OTUs. But OTUS also gives this!
+  
+  ##############!!!!!!!!!!!!!!!!!!!!!!!!!!!#########################!!!!!!!!!!!!!!!!!!!!
+  #Now: Work out how to add the matching Taxonomy from tax to OTUnames, in the order of OTUnames. Make THIS new thing famNames
+  
   famNames = unlist(lapply(famNames, function(fam) ifelse((is.na(fam) | fam == "unclassified"), yes="UATL", no=fam)))
   famNames.collapsed = as.factor(famNames)
   levels(famNames.collapsed) = c(levels(famNames.collapsed), "Other")
