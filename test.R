7e83bfc72634c1a2eae4a83ce247ccbb5a577b3e
moanam <moanam@clear.net.nz>
2016-11-22 10:42:35 -0800

    Updated notes (as I fixed the earlier mistake)

--- test.R
+++ test.R
@@ -3,4 +3,4 @@
 # mat2plot is the matrix of heat map data, with the OTUs in order (41, atm)
 mat2plot = get(paste(replicate, ".abs.subset.smooth.norm.ordered.rm", sep=""))
 # .abs.subset.smooth.norm.ordered.rm has the OTU names as row names. I can probably rearrange this.
-#OTUnames links the OTU with the family! But only for the last replicate. I have a mistake in the code! This means that all the lagends have been for the last replicate.
+#M.OTUnames links the OTU with the family
